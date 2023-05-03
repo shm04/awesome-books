08228a51e2580f49ee49e4bf144fbd51e9608efe
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-05-03 14:41:36 -0600

    Refactor javascript file with classes instead of arrays or objects

--- script.js
+++ script.js
@@ -1,47 +1,61 @@
+/* eslint-disable max-classes-per-file */
+
 const inputOne = document.querySelector('.input-one');
 const inputTwo = document.querySelector('.input-two');
 const addButton = document.querySelector('.add-button');
 const booksSection = document.querySelector('.books-section');
 
-let bookList = [];
-
-const writeBook = () => {
-  booksSection.innerHTML = '';
-
-  const list = document.createElement('ul');
-  list.classList.add('books-list');
-
-  bookList.forEach((book, index) => {
-    const li = document.createElement('li');
-    li.classList.add('list-item');
-    li.innerHTML = `
-    <h2 class="li-title">${book.title}</h2>
-    <p class="li-text">${book.author}</p>
-    <button class="remove-btn" id="${index}">Remove</button>
-    `;
-
-    list.appendChild(li);
-    booksSection.appendChild(list);
-
-    const removeBook = (title) => {
-      bookList = bookList.filter((book) => book.title !== title);
-      localStorage.setItem('bookList', JSON.stringify(bookList));
-      writeBook();
-    };
-
-    const removeBtn = document.getElementById(`${index}`);
-    removeBtn.addEventListener('click', () => removeBook(book.title));
-  });
-};
-
-if (localStorage.getItem('bookList')) {
-  bookList = JSON.parse(localStorage.getItem('bookList'));
-  writeBook();
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 
-addButton.addEventListener('click', () => {
-  const newBook = { title: `${inputOne.value}`, author: `${inputTwo.value}` };
+class Books {
+  constructor() {
+    this.bookList = [];
+    if (localStorage.getItem('bookList')) {
+      this.bookList = JSON.parse(localStorage.getItem('bookList'));
+    }
+  }
 
+  addBook(book) {
+    this.bookList.push(book);
+    localStorage.setItem('bookList', JSON.stringify(this.bookList));
+    this.writeBooks();
+  }
+
+  removeBook(title) {
+    this.bookList = this.bookList.filter((book) => book.title !== title);
+    localStorage.setItem('bookList', JSON.stringify(this.bookList));
+    this.writeBooks();
+  }
+
+  writeBooks() {
+    booksSection.innerHTML = '';
+    const list = document.createElement('ul');
+    list.classList.add('books-list');
+    this.bookList.forEach((book, index) => {
+      const li = document.createElement('li');
+      li.classList.add('list-item');
+      li.innerHTML = `
+        <h2 class="li-title">${book.title}</h2>
+        <p class="li-text">${book.author}</p>
+        <button class="remove-btn" id="${index}">Remove</button>
+      `;
+      list.appendChild(li);
+      booksSection.appendChild(list);
+
+      const removeBtn = document.getElementById(`${index}`);
+      removeBtn.addEventListener('click', () => this.removeBook(book.title));
+    });
+  }
+}
+
+const books = new Books();
+
+addButton.addEventListener('click', () => {
   const inputOneValue = inputOne.value;
   const inputTwoValue = inputTwo.value;
 
@@ -55,11 +69,12 @@ addButton.addEventListener('click', () => {
     errorMsg.innerHTML = 'Please fill both inputs';
     booksSection.appendChild(errorMsg);
   } else {
-    bookList.push(newBook);
-    localStorage.setItem('bookList', JSON.stringify(bookList));
-    writeBook();
+    const newBook = new Book(inputOneValue, inputTwoValue);
+    books.addBook(newBook);
   }
 
   inputOne.value = '';
   inputTwo.value = '';
 });
+
+books.writeBooks();
